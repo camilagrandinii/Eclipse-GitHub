a274e6e5ad5e81c512c3a0be4e530cde7158858b
Camila <cacagrandini@gmail.com>
2021-10-05 09:09:29 -0300

    Added pom.xml

---
+++ 03ConexãoPostgres/maven.demo2/src/main/java/conexao2/maven/demo2/App.java
@@ -0,0 +1,13 @@
+package conexao2.maven.demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
